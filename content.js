6c590801ee9d91b1a178852b850fcb7175fc9465
Erin Bleiweiss <erin.bleiweiss@utexas.edu>
2015-12-14 21:34:07 -0600

    options menu

--- content.js
+++
@@ -1,6 +0,0 @@
-var textbox = $(".md textarea");
-
-var isCFB = window.location.href.toLowerCase().indexOf("/r/cfb/comments") > -1;
-if (isCFB){
-    textbox.val("\n\nhello world");
-}
--- popup.js
+++
@@ -1,6 +0,0 @@
-//document.addEventListener('DOMContentLoaded', function(){
-//    var isReddit = window.location.href.indexOf("reddit.com") > -1;
-//
-//    console.log(isReddit);
-//
-//}, false);
