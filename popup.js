de8e5d46d41c4b2c7d6056dac8c056b505cd3283
Erin Bleiweiss <erin.bleiweiss@utexas.edu>
2015-12-15 10:44:30 -0600

    add/remove multiple signatures

---
+++ popup.js
@@ -0,0 +1,11 @@
+$("#onoff").click(function(){
+    console.log($("#onoff").checked)
+});
+
+
+//document.addEventListener('DOMContentLoaded', function(){
+//    var isReddit = window.location.href.indexOf("reddit.com") > -1;
+//
+//    console.log(isReddit);
+//
+//}, false);
